fc56f95de1b264f123492384612c672801302743
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2023-05-22 01:57:37 +0300

    update

--- app/Console/Commands/ScrapDomains.php
+++ app/Console/Commands/ScrapDomains.php
@@ -32,58 +32,11 @@ class ScrapDomains extends Command
     public function handle()
     {
 
-        $getDomains = Domain::all();
-        foreach ($getDomains as $domain) {
-
-            try {
-                $domain = 'https://' . $domain->domain;
-
-                $client = new Client(['base_uri' => $domain]);
-                $getRequest = $client->request('GET');
-                $content = $getRequest->getBody()->getContents();
-
-                libxml_use_internal_errors(true);
-
-                $dom = new \DOMDocument();
-                $dom->loadHTML('<!DOCTYPE html><meta charset="UTF-8">' . $content);
-
-                foreach ($dom->getElementsByTagName('a') as $node) {
-                    $href = $node->getAttribute('href');
-                    if (strpos($href, 'http') !== false) {
-
-                        $domain = UrlHelper::getDomainFromUrl($href);
-                        if (empty(trim($domain))) {
-                            continue;
-                        }
-
-                        if (!UrlHelper::isValidDomainName($domain)) {
-                            continue;
-                        }
-
-                        if (UrlHelper::isSubDomainName($domain)) {
-                            continue;
-                        }
-
-                        $findDomain = Domain::where('domain', $domain)->first();
-                        if ($findDomain == null) {
-                            $findDomain = new Domain();
-                            $findDomain->domain = $domain;
-                        }
-                        $findDomain->save();
-
-                        $findWebsiteLink = WebsiteLink::where('website_link', $href)->first();
-                        if ($findWebsiteLink == null) {
-                            $findWebsiteLink = new WebsiteLink();
-                            $findWebsiteLink->website_link = $href;
-                        }
-
-                        $findWebsiteLink->save();
-
-                    }
-                }
-            } catch (\Exception $e) {
-                echo $e->getMessage() . "\n";
-                continue;
+        while (true) {
+            $getDomains = Domain::all();
+            foreach ($getDomains as $domain) {
+                \App\ScrapDomains::scrapDomain($domain->domain);
+                sleep(rand(1,4));
             }
         }
 
---
+++ app/ScrapDomains.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use App\Models\Domain;
+use App\Models\WebsiteLink;
+use GuzzleHttp\Client;
+
+class ScrapDomains
+{
+    public static function scrapDomain($domain)
+    {
+        try {
+            $domain = 'https://' . $domain;
+
+            $client = new Client(['base_uri' => $domain]);
+            $getRequest = $client->request('GET');
+            $content = $getRequest->getBody()->getContents();
+
+            libxml_use_internal_errors(true);
+
+            $dom = new \DOMDocument();
+            $dom->loadHTML('<!DOCTYPE html><meta charset="UTF-8">' . $content);
+
+            foreach ($dom->getElementsByTagName('a') as $node) {
+                $href = $node->getAttribute('href');
+                if (strpos($href, 'http') !== false) {
+
+                    $domain = UrlHelper::getDomainFromUrl($href);
+                    if (empty(trim($domain))) {
+                        continue;
+                    }
+
+                    if (!UrlHelper::isValidDomainName($domain)) {
+                        continue;
+                    }
+
+                    if (UrlHelper::isSubDomainName($domain)) {
+                        continue;
+                    }
+
+                    $findDomain = Domain::where('domain', $domain)->first();
+                    if ($findDomain == null) {
+                        $findDomain = new Domain();
+                        $findDomain->domain = $domain;
+                    }
+                    $findDomain->save();
+
+                    $findWebsiteLink = WebsiteLink::where('website_link', $href)->first();
+                    if ($findWebsiteLink == null) {
+                        $findWebsiteLink = new WebsiteLink();
+                        $findWebsiteLink->website_link = $href;
+                    }
+
+                    $findWebsiteLink->save();
+
+                }
+            }
+        } catch (\Exception $e) {
+            echo $e->getMessage() . "\n";
+        }
+    }
+}
